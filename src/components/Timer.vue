48ba7dbaa406e8ab4e4e5e3856aecfe3d69e9bc5
yuki-yoshii <yuki-yoshii@navitime.co.jp>
2022-09-30 16:00:40 +0900

    パス修正

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -61,7 +61,8 @@ const resetTimer = (pausesAudio: boolean = false): void => {
   audio.currentTime = 0;
 };
 
-const audio = new Audio("./src/assets/alarm.mp3");
+//FIXME path が開発環境と本番環境での違いを吸収できていない
+const audio = new Audio("./assets/alarm.mp3");
 watch(sec, (): void => {
   if (!isTimerStopped.value && sec.value > 0) return;
   audio.play();
